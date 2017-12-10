a59e6c6c27f1d5297544bd4ee44556c610d9ff9d
Kellie Dinh <dinhkellie@yahoo.com>
2017-12-10 17:05:45 -0500

    finish layout with track list

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var philly = new Audio();
 philly.src = "https://cdn.glitch.com/fa419486-c10f-4f12-bf62-aacd1e70bc21%2FBruce%20Springsteen%20-%20Streets%20Of%20Philadelphia.mp3?1512372770613";
+
 function PlaySound() {
     if (philly.paused) {
       philly.play();
